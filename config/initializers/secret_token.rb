9b8e11dddb420e1e62c39580a4f106fa7e8c8dad
jabberW00KY <dgrigoriev2@gmail.com>
2014-03-12 19:58:48 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ivt::Application.config.secret_key_base = '0c0f0591ceb04d2bf49d0f01d2fc195911273512913e62e9d30753fd3ff3be55ddd91a2d00bd4a3562b88c7dc7ad7f5657247abdd892fc03532ad9c00fd7b975'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ivt::Application.config.session_store :cookie_store, key: '_ivt_session'
