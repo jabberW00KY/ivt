674d5efab453fcec1765d7ae0ec965eedecf6270
jabberW00KY <dgrigoriev2@gmail.com>
2014-04-05 01:55:59 +0400

    Application.slim changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ivt::Application.routes.draw do
   resources :news
-
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
